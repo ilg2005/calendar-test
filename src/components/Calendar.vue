555eecf9082b761abffa2060803f29e3cded3bd0
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-03-28 21:58:18 +0300

    стилизация кнопок календаря

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,23 +1,28 @@
 <script setup>
-import { useCalendarStore } from '../stores/calendarStore';
-import { format } from 'date-fns';
-import { ru } from 'date-fns/locale';
-import { computed } from 'vue';
+import {useCalendarStore} from '../stores/calendarStore';
+import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/vue/24/solid'
+import {format} from 'date-fns';
+import {ru} from 'date-fns/locale';
+import {computed} from 'vue';
 
 const calendarStore = useCalendarStore();
 
-const weekDates = computed(() => calendarStore.weekDates.map(date => format(date, 'eee, MMM d', { locale: ru })));
+const weekDates = computed(() => calendarStore.weekDates.map(date => format(date, 'eee, MMM d', {locale: ru})));
 </script>
 
 <template>
   <div class="flex items-center space-x-2">
-    <button @click="calendarStore.toPrevWeek()" class="btn-calendar">&lt;</button>
+    <button class="btn-calendar" @click="calendarStore.toPrevWeek()">
+      <ChevronLeftIcon class="h-5 w-5 scale-150"/>
+    </button>
     <div class="flex overflow-x-auto">
       <div v-for="(date, index) in weekDates" :key="index" class="px-4 py-2">
         {{ date }}
       </div>
     </div>
-    <button @click="calendarStore.toNextWeek()" class="btn-calendar">&gt;</button>
+    <button class="btn-calendar" @click="calendarStore.toNextWeek()">
+      <ChevronRightIcon class="h-5 w-5 scale-150"/>
+    </button>
   </div>
 </template>
 
