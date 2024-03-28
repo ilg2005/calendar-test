2d66f33549c4a6e22d7970acf98d900b5e5dcbd3
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-03-28 21:32:15 +0300

    стилизация кнопок календаря

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -11,13 +11,13 @@ const weekDates = computed(() => calendarStore.weekDates.map(date => format(date
 
 <template>
   <div class="flex items-center space-x-2">
-    <button @click="calendarStore.toPrevWeek()" class="">&lt;</button>
+    <button @click="calendarStore.toPrevWeek()" class="btn-calendar">&lt;</button>
     <div class="flex overflow-x-auto">
       <div v-for="(date, index) in weekDates" :key="index" class="px-4 py-2">
         {{ date }}
       </div>
     </div>
-    <button @click="calendarStore.toNextWeek()" class="">&gt;</button>
+    <button @click="calendarStore.toNextWeek()" class="btn-calendar">&gt;</button>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-brown': '#a52a2a',
+      },
+    },
   },
   plugins: [],
 }
