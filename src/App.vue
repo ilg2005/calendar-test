d57e83fc3478c16ff79af5c5611c03c0f7759855
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-03-28 18:37:40 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-orange-600">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
