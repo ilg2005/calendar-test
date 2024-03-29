ae70b0257b87d53bb66511c1db4b431d036c9f05
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-03-29 17:18:52 +0300

    добавлено открытие модального окна по клику на строку с объявлением

---
+++ src/components/AdModal.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {XMarkIcon} from '@heroicons/vue/24/outline';
+import {format} from "date-fns";
+
+defineProps({
+  ad: Object,
+  isOpen: Boolean
+});
+
+const emit = defineEmits(['close']);
+
+const closeModal = () => {
+  emit('close');
+};
+</script>
+
+<template>
+  <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center">
+    <div class="bg-white p-4 rounded-md shadow-2xl min-w-[300px]">
+      <div class="flex flex-col">
+        <button class="self-end -mr-3 -mt-3" @click="closeModal">
+          <XMarkIcon class="w-3 h-3 hover:scale-125 text-gray-500"/>
+        </button>
+        <h2 class="text-sm text-gray-500 mb-3">Объявление #{{ ad.id }}</h2>
+        <hr class="-mt-2 mb-2">
+        <p class="mb-2 text-blue-700 ">{{ ad.title }}</p>
+        <p class="text-xs italic text-gray-500 self-end">{{ format(ad.date, 'd.MM.yyyy') }}</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/AdsList.vue
+++ src/components/AdsList.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {computed, onMounted} from 'vue';
+import {computed, onMounted, ref} from 'vue';
 import {useAdsStore} from '@/stores/adsStore';
 import {useCalendarStore} from '@/stores/calendarStore';
+import AdModal from "@/components/AdModal.vue";
 
 const adsStore = useAdsStore();
 const calendarStore = useCalendarStore();
@@ -16,9 +17,19 @@ const adsForSelectedDate = computed(() => {
   }
   return adsStore.adsByDate(calendarStore.selectedDate);
 });
+
+const selectedAd = ref(null);
+const isModalOpen = ref(false);
+
+const openModal = (ad) => {
+  selectedAd.value = ad;
+  isModalOpen.value = true;
+};
 </script>
 
 <template>
+  <AdModal v-if="isModalOpen" :ad="selectedAd" :isOpen="isModalOpen" @close="isModalOpen = false"/>
+
   <p v-if="calendarStore.selectedDate" class="pb-4">Найдено объявлений: {{ adsForSelectedDate.length }}</p>
 
 
@@ -28,7 +39,8 @@ const adsForSelectedDate = computed(() => {
     <tr v-for="(ad, index) in adsForSelectedDate"
         :key="ad.id"
         :class="{ 'bg-gray-50': index % 2 }"
-        class="hover:bg-gray-100"
+        class="cursor-pointer hover:bg-gray-100"
+        @click="openModal(ad)"
     >
       <td class="table-cell">
         <div class="text-gray-900">#{{ ad.id }}</div>
